929dfc87d788fcea239d245df2b9ee92dbbd268b
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-10-21 11:47:15 +0300

    Part 13 Exercise 3

---
+++ cli.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const Blog = require('./models/blog')
+
+const sequelize = require('./db')
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    const blogs = await Blog.findAll()
+    blogs.forEach((blog) => {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    })
+    sequelize.close()
+  } catch (e) {
+    console.error('Unable to connect', e)
+  }
+}
+
+main()
